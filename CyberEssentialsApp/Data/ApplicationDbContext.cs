f08a712e63477b9f7ea9e4b8056f02119abdd966
jacobrphillips <0123phillips@gmail.com>
2024-11-24 16:55:07 +0000

    Creates the ApplicationDbContext class

---
+++ CyberEssentialsApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CyberEssentialsApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CyberEssentialsApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Workstation> Workstations { get; set; }
+        public DbSet<Server> Servers { get; set; }
+        public DbSet <Mobile> Mobiles { get; set; }
+        public DbSet <Firewall> Firewalls { get; set; }
+
+    }
+}
