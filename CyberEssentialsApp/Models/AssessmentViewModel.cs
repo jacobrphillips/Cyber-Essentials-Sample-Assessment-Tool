b991cdeca83c82bb7a8d73917d256c267f0a3352
jacobrphillips <0123phillips@gmail.com>
2024-11-24 15:09:15 +0000

    Adjusts AssessmentViewModel to include Firewalls and TotalFirewalls properties

--- CyberEssentialsApp/Models/AssessmentViewModel.cs
+++ CyberEssentialsApp/Models/AssessmentViewModel.cs
@@ -5,9 +5,11 @@
         public List<Workstation> Workstations { get; set; }
         public List<Server> Servers { get; set; }
         public List<Mobile> Mobiles { get; set; }
+        public List<Firewall> Firewalls { get; set; }
         public int TotalWorkstations { get; set; }
         public int TotalServers { get; set; }
         public int TotalMobiles { get; set; }
+        public int TotalFirewalls { get; set; }
         public int RequiredSampleSize { get; set; }
     }
 }
